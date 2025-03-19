a60e60a3ceda5cd32d7d54a9a0e7c46690b590d4
Albert Badia Luján <albert.badial@estudiants.urv.cat>
2025-03-19 08:50:57 +0100

    feat: added 3 necessary files to start implementing

---
+++ InsultService.py
@@ -0,0 +1,14 @@
+import xmlrpc
+import redis
+
+# Connect to Redis
+client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+queue_name = ["insults"]
+
+print("Consumer is waiting for tasks...")
+
+while True:
+    task = client.blpop(queue_name, timeout=0)  # Blocks indefinitely until a task is available
+    if task:
+        print(f"Consumed: {task[1]}")
---
+++ client1.py
@@ -0,0 +1,15 @@
+import redis
+from time import sleep
+
+# Connect to Redis
+client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+queue_name = "task_queue"
+
+# Send multiple messages
+tasks = ["Task 1", "Task 2", "Task 3"]
+
+for task in tasks:
+    client.rpush(queue_name, task)
+    print(f"Produced: {task}")
+    sleep(1)  # Simulating a delay in task production
