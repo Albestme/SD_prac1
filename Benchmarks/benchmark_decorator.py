1c4a464722efa154009ae2b0bcf24b3b067de95f
Albert Badia Luján <albert.badial@estudiants.urv.cat>
2025-05-06 09:16:25 +0200

    feat: implemented the filter service decorator to make tests

--- Benchmarks/benchmark_decorator.py
+++ Benchmarks/benchmark_decorator.py
@@ -2,6 +2,7 @@ import time
 import multiprocessing
 import random
 from XMLRPC.insult_service import InsultService as Xmlrpc_Insult_Service
+from XMLRPC.insult_filter import InsultFilter as Xmlrpc_Filter_Service
 import csv
 
 
@@ -17,6 +18,15 @@ class BenchmarkDecorator:
         self.end_time = None
         self.architecture = architecture    # XMLRPC, Redis, RabbitMQ_Redis, Pyro
         self.mode = mode                    # single_node, multi_node_static, multi_node_dynamic
+
+    def create_clients(self, num_clients, iterations_per_client):
+        processes = []
+        for i in range(num_clients):
+            processes.append(multiprocessing.Process(target=self.stressfull_client, args=(iterations_per_client, )))
+            processes[i].start()
+
+        for process in processes:
+            process.join()
 
 
 class InsultServiceDecorator(BenchmarkDecorator):
@@ -34,7 +44,7 @@ class InsultServiceDecorator(BenchmarkDecorator):
                         ]
 
     def stressfull_client(self, iterations):
-        for _ in range(0, iterations):
+        for _ in range(iterations):
             self.insult_service.add_insult(random.choice(self.insults))
 
     def stress_insult_service(self, num_clients=3, iterations_per_client=2500000):
@@ -49,19 +59,46 @@ class InsultServiceDecorator(BenchmarkDecorator):
         print(f'Elapsed time: {total_time} seconds')
         return total_time
 
-    def create_clients(self, num_clients, iterations_per_client):
-        processes = []
-        for i in range(num_clients):
-            processes.append(multiprocessing.Process(target=self.stressfull_client, args=(iterations_per_client, )))
-            processes[i].start()
 
-        for process in processes:
-            process.join()
+class FilterServiceDecorator(BenchmarkDecorator):
+    def __init__(self, service, architecture, mode):
+        super().__init__(architecture, mode)
+        self.filter_service = service
+        self.texts = [
+                        'JAJAJA, Im sorry, but you are dumb and stupid',
+                        'I love to see a moron cry',
+                        'stupid idiot I hate you',
+                        'Only a groomer would say that',
+                        'I love to way acrotomophile cry',
+                        'Que viva España',
+                        'You are an air head and an accident',
+                        'I love how strawberries taste'
+                      ]
+
+    def stressfull_client(self, iterations):
+        for _ in range(iterations):
+            self.filter_service.append_text_filtering_work_queue(random.choice(self.texts))
+
+    def stress_filter_service(self, num_clients=3, iterations_per_client=2500000):
+        self.start_time = time.time()
+        self.create_clients(num_clients, iterations_per_client)
+        self.end_time = time.time()
+        total_time = self.end_time - self.start_time
+
+        write_csv(self.mode, self.architecture, num_clients, iterations_per_client, total_time)
+
+        print(f'Elapsed time: {total_time} seconds')
+        return total_time
+
+
 
 
 if __name__ == "__main__":
     (InsultServiceDecorator(Xmlrpc_Insult_Service(), 'XMLRPC', 'single_node' ).
      stress_insult_service(num_clients=10, iterations_per_client=10000000))
+
+    (FilterServiceDecorator(Xmlrpc_Filter_Service(), 'XMLRPC', 'single_node').
+     stress_filter_service(num_clients=10, iterations_per_client=10000000))
 
 
 
