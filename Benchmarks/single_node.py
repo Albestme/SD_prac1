459e256e342715decba5fe1fd114f0b36a73e852
Albert Badia Luján <albert.badial@estudiants.urv.cat>
2025-05-08 11:06:46 +0200

    fix: corrected how the XMLRPC and Pyro servers are accessed

--- Benchmarks/single_node.py
+++ Benchmarks/single_node.py
@@ -1,6 +1,8 @@
 import time
 import csv
 import redis
+import xmlrpc.client
+import Pyro4
 from benchmark_decorator import InsultServiceDecorator, FilterServiceDecorator
 from XMLRPC.insult_service import InsultService as Xmlrpc_Insult_Service
 from XMLRPC.insult_filter import InsultFilter as Xmlrpc_Filter_Service
@@ -14,21 +16,30 @@ from Pyro.insult_filter import FilterService as Pyro_Filter_Service
 
 def benchmark_single(module_names, clients, iterations):
     for module_name in module_names:
+        print("starting benchmark for module:", module_name)
         if module_name == 'XMLRPC':
-            (InsultServiceDecorator(Xmlrpc_Insult_Service(), 'XMLRPC', 'single_node')
+            insult_service_proxy = xmlrpc.client.ServerProxy('http://localhost:8000')
+            filter_service_proxy = xmlrpc.client.ServerProxy('http://localhost:8001')
+            (InsultServiceDecorator(insult_service_proxy, 'XMLRPC', 'single_node')
             .stress_insult_service(num_clients=clients, iterations_per_client=iterations))
-            (FilterServiceDecorator(Xmlrpc_Filter_Service(), 'XMLRPC', 'single_node')
+            (FilterServiceDecorator(filter_service_proxy, 'XMLRPC', 'single_node')
             .stress_filter_service(num_clients=clients, iterations_per_client=iterations))
+
         elif module_name == 'Pyro':
-            (InsultServiceDecorator(Pyro_Insult_Service(), 'Pyro', 'single_node')
+            ns = Pyro4.locateNS()
+            insult_service_proxy = Pyro4.Proxy(ns.lookup("insult.service"))
+            filter_service_proxy = Pyro4.Proxy(ns.lookup("filter.service"))
+            (InsultServiceDecorator(insult_service_proxy, 'Pyro', 'single_node')
             .stress_insult_service(num_clients=clients, iterations_per_client=iterations))
-            (FilterServiceDecorator(Pyro_Filter_Service(), 'Pyro', 'single_node')
+            (FilterServiceDecorator(filter_service_proxy, 'Pyro', 'single_node')
             .stress_filter_service(num_clients=clients, iterations_per_client=iterations))
+
         elif module_name == 'Redis':
             (InsultServiceDecorator(Redis_Insult_Service(), 'Redis', 'single_node')
             .stress_insult_service(num_clients=clients, iterations_per_client=iterations))
             (FilterServiceDecorator(Redis_Filter_Service(), 'Redis', 'single_node')
             .stress_filter_service(num_clients=clients, iterations_per_client=iterations))
+
         elif module_name == 'RabbitMQ_Redis':
             (InsultServiceDecorator(RabbitMQ_Insult_Service(), 'RabbitMQ_Redis', 'single_node')
             .stress_insult_service(num_clients=clients, iterations_per_client=iterations))
@@ -38,7 +49,7 @@ def benchmark_single(module_names, clients, iterations):
 
 if __name__ == "__main__":
     # Define the module name
-    architectures = ['XMLRPC', 'Redis', 'RabbitMQ_Redis', 'Pyro']
+    architectures = ['XMLRPC']
 
-    benchmark_single(architectures, 3, 10000000)
+    benchmark_single(architectures, 3, 10000)
 
