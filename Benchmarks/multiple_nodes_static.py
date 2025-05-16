0c4a2f19f8c0e7d4cd548c6f132c5a12a3b15672
Albert Badia Luján <albert.badial@estudiants.urv.cat>
2025-05-16 09:47:31 +0200

    feat: first load balancer implementation and multiple_nodes_static.py

---
+++ Benchmarks/multiple_nodes_static.py
@@ -0,0 +1,82 @@
+import xmlrpc.client
+import Pyro4
+from benchmark_decorator import InsultServiceDecorator, FilterServiceDecorator
+from Redis.insult_service import InsultService as Redis_Insult_Service
+from Redis.insult_filter import InsultFilter as Redis_Filter_Service
+from RabbitMQ_Redis.insult_service import InsultService as RabbitMQ_Insult_Service
+from RabbitMQ_Redis.insult_filter import InsultFilter as RabbitMQ_Filter_Service
+from loadbalancer import LoadBalancer
+
+
+def benchmark_multi_node_static(module_names, clients, iterations, nodes):
+    for module_name in module_names:
+        print("starting benchmark for module:", module_name)
+        if module_name == 'XMLRPC':
+            lb = connect_servers(module_name, nodes)
+            lb.stress_insult_service(clients, iterations)
+            lb.stress_filter_service(clients, iterations)
+
+        elif module_name == 'Pyro':
+            lb = connect_servers(module_name, nodes)
+            lb.stress_insult_service(clients, iterations)
+            lb.stress_filter_service(clients, iterations)
+
+        elif module_name == 'Redis':
+            lb = connect_servers(module_name, nodes)
+            lb.stress_insult_service(clients, iterations)
+            lb.stress_filter_service(clients, iterations)
+
+        elif module_name == 'RabbitMQ_Redis':
+            lb = connect_servers(module_name, nodes)
+            lb.stress_insult_service(clients, iterations)
+            lb.stress_filter_service(clients, iterations)
+
+
+def connect_servers(module_name, nodes):
+    if module_name == 'XMLRPC':
+        lb = LoadBalancer('XMLRPC', 'multi_node_static')
+        for i in range(nodes):
+            insult_service_proxy = xmlrpc.client.ServerProxy(f'http://localhost:{8000 + i}')
+            filter_service_proxy = xmlrpc.client.ServerProxy(f'http://localhost:{8000 + nodes + i}')
+            lb.register_insult_service(insult_service_proxy)
+            lb.register_filter_service(filter_service_proxy)
+        return lb
+
+    elif module_name == 'Pyro':
+        lb = LoadBalancer('Pyro', 'multi_node_static')
+        ns = Pyro4.locateNS()
+        for i in range(nodes):
+            insult_service_proxy = Pyro4.Proxy(ns.lookup(f"insult.service.{i}"))
+            filter_service_proxy = Pyro4.Proxy(ns.lookup(f"filter.service.{i}"))
+            lb.register_insult_service(insult_service_proxy)
+            lb.register_filter_service(filter_service_proxy)
+        return lb
+
+    elif module_name == 'Redis':
+        lb = LoadBalancer('Redis', 'multi_node_static')
+        for i in range(nodes):
+            insult_service = Redis_Insult_Service()
+            filter_service = Redis_Filter_Service()
+            lb.register_insult_service(insult_service)
+            lb.register_filter_service(filter_service)
+
+        return lb
+
+    elif module_name == 'RabbitMQ_Redis':
+        lb = LoadBalancer('RabbitMQ_Redis', 'multi_node_static')
+        for i in range(nodes):
+            insult_service = RabbitMQ_Insult_Service()
+            filter_service = RabbitMQ_Filter_Service()
+            lb.register_insult_service(insult_service)
+            lb.register_filter_service(filter_service)
+
+        return lb
+
+    return None
+
+
+if __name__ == '__main__':
+    # Define the module name
+    architectures = ['Redis', 'RabbitMQ_Redis']
+
+    benchmark_multi_node_static(architectures, 3, 20000, 2)
--- loadbalancer.py
+++ loadbalancer.py
@@ -1,24 +1,46 @@
 import random
+import time
+from Benchmarks.benchmark_decorator import InsultServiceDecorator, FilterServiceDecorator
+import multiprocessing
+from Benchmarks.benchmark_decorator import write_csv
 
 # Proxy that attends the requests of the clients
-# Singleton class
 class LoadBalancer:
-    _instance = None
-    _lock = object()
+    insult_services = []
+    filter_services = []
+    texts = [
+        'JAJAJA, Im sorry, but you are dumb and stupid',
+        'I love to see a moron cry',
+        'stupid idiot I hate you',
+        'Only a groomer would say that',
+        'I love to way acrotomophile cry',
+        'Que viva España',
+        'You are an air head and an accident',
+        'I love how strawberries taste'
+    ]
+    insults = ['dumb',
+                        'moron',
+                        'stupid',
+                        'idiot',
+                        'groomer',
+                        'acrotomophile',
+                        'air head',
+                        'accident'
+                        ]
 
-    def __init__(self):
-        # The initialization is done in __new__
-        self.filter_services = []
-        self.insult_services = []
+    def __init__(self, architecture, mode):
+        self.architecture = architecture
+        self.mode = mode
 
     def register_insult_service(self, service):
+        service = InsultServiceDecorator(service, self.architecture, self.mode)
         self.insult_services.append(service)
 
     def register_filter_service(self, service):
+        service = FilterServiceDecorator(service, self.architecture, self.mode)
         self.filter_services.append(service)
 
     def add_insult(self, insult):
-        # Randomly select an insult service to handle the request
         service = random.choice(self.insult_services)
         return service.add_insult(insult)
 
@@ -26,5 +48,40 @@ class LoadBalancer:
         service = random.choice(self.filter_services)
         return service.append_text_filtering_work_queue(text)
 
+    def stressfull_client(self, iterations, service_type):
+        if service_type == 'insult':
+            for _ in range(iterations):
+                service = random.choice(self.insult_services).insult_service
+                service.add_insult(random.choice(self.insults))
+        elif service_type == 'filter':
+            for _ in range(iterations):
+                service = random.choice(self.filter_services).filter_service
+                service.append_text_filtering_work_queue(random.choice(self.texts))
 
+    def stress_insult_service(self, num_clients, iterations_per_client):
+        start_time = time.time()
+        processes = []
+        for i in range(num_clients):
+            processes.append(
+                multiprocessing.Process(target=self.stressfull_client, args=(iterations_per_client, 'insult', )))
+            processes[i].start()
 
+        for p in processes:
+            p.join()
+        end_time = time.time()
+        total_time = end_time - start_time
+        write_csv(self.mode, self.architecture, num_clients, iterations_per_client, total_time, 'insult_service')
+
+    def stress_filter_service(self, num_clients, iterations_per_client):
+        start_time = time.time()
+        processes = []
+        for i in range(num_clients):
+            processes.append(
+                multiprocessing.Process(target=self.stressfull_client, args=(iterations_per_client, 'filter', )))
+            processes[i].start()
+
+        for p in processes:
+            p.join()
+        end_time = time.time()
+        total_time = end_time - start_time
+        write_csv(self.mode, self.architecture, num_clients, iterations_per_client, total_time, 'filter_service')
